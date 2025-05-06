e51e06f5dc26e3ce1beb93f0f0c805f1938f1720
Lucas Arouca <lucasarouca2002@gmail.com>
2025-05-06 17:14:19 -0300

    teste

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'App está rodando!'
 
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    return f'Código de autorização recebido: {code}'
+    token_url = "https://api.digikey.com/v1/oauth2/token"
+    data = {
+        "client_id": "SEU_CLIENT_ID",
+        "client_secret": "SEU_CLIENT_SECRET",
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": "https://api.lucasdanconia.com/callback"
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get('access_token')
+    # Salve o token ou use-o imediatamente
+    return "Autenticação concluída! Token: " + access_token
